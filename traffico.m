508d0a244f9a2c702b4b56e9681240896b4711a7
tommaso1311 <tommaso131195@gmail.com>
2018-10-10 00:13:39 +0200

    riscritta da capo la creazione della matrice di adiacenza: ora si utilizza un metodo che si basa sulla distanza per creare tutti i link che poi vengono rimossi in modo random

--- traffico.m
+++ traffico.m
@@ -3,29 +3,43 @@ close all;
 clc;
 
 rng('shuffle');
+PCross = 0.75;
 
-NRoads = 49;
-CitySize = sqrt(NRoads);
+City_x = 10;
+City_y = 10;
 
-NCars = 30;
+distFunc = 'cityblock'; % 4-connect
+% distFunc = 'chebychev'; % 8-connect
 
-PCross = 0.8;
+[X, Y] = meshgrid(1:City_x, 1:City_y);
+X = X(:);
+Y = Y(:);
 
-Adj = zeros(NRoads);
+Adj = squareform(pdist([X, Y], distFunc) == 1);
+Rem = triu(rand(City_x*City_y), 1) < PCross;
+Rem = Rem & Rem';
+Adj = Adj & Rem;
 
-for i = 1:NRoads
-    for j = i+1:NRoads
-    
-    x = rand;
-    if ismember(j, [i+1, i+CitySize, i+CitySize+1]) && x < PCross
-        
-        Adj(i, j) = 1;
-    end;
-    
-    end;
-end;
+subplot(121), spy(Adj)
 
-Adj = Adj | Adj';
+[xx yy] = gplot(Adj, [X Y]);
+subplot(122), plot(xx, yy, 'ks-', 'MarkerFaceColor','r')
+axis([0 City_x+1 0 City_y+1])
+%# add labels
+[X Y] = meshgrid(1:City_x,1:City_y);
+X = reshape(X',[],1) + 0.1; Y = reshape(Y',[],1) + 0.1;
+text(X, Y(end:-1:1), cellstr(num2str((1:City_x*City_y)')) )
 
-G = graph(Adj);
-plot(G)
+% G = graph(Adj);
+% plot(G);
+% imagesc(Adj);
+
+% y = floor(randfixedsum(NRoads, 1, NCars, 0, NCars))
+
+
+
+
+
+
+
+
